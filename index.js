754de1c5c4459d979ae07e243cc09b1545ab637c
Raul Gutierrez <87869231+rgtz18@users.noreply.github.com>
2023-08-09 15:50:27 -0500

    Updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>HERE WE GO!!!!</h1>);
+root.render(<h1>HERE WE GO React!!!!</h1>);
 
 window.addEventListener("load", (event) => {
     $("#navHeader").load("navigation.html");
