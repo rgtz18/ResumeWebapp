b3afc5bcd0a9303b7b43381e3f31dedba9cff205
Raul Gutierrez <87869231+rgtz18@users.noreply.github.com>
2023-08-09 15:46:24 -0500

    Attempting to add react

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import { createRoot } from 'react-dom/client';
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>HERE WE GO!!!!</h1>);
+
 window.addEventListener("load", (event) => {
     $("#navHeader").load("navigation.html");
 });
