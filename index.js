46cec2998d8f2d8fe9868b0bdbc1823c60d55e17
Raul Gutierrez <87869231+rgtz18@users.noreply.github.com>
2023-08-14 11:44:23 -0500

    omitn react code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { createRoot } from 'react-dom/client';
+//import { createRoot } from 'react-dom/client';
 
 // Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+//document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>HERE WE GO React!!!!</h1>);
+//const root = createRoot(document.getElementById('app'));
+//root.render(<h1>HERE WE GO React!!!!</h1>);
 
 window.addEventListener("load", (event) => {
     $("#navHeader").load("navigation.html");
